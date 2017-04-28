b98f9a54f6f0342fc4d02060b58570f967be661e
Coty Embry <cotyembry@gmail.com>
2017-04-27 19:02:42 -0500

    first commit

---
+++ logonCommands.cmd
@@ -0,0 +1,17 @@
+DOSKEY ls=dir
+DOSKEY del=rm
+DOSKEY c=cls
+DOSKEY clear=cls
+DOSKEY nrw=npm run webpack
+DOSKEY nrs=npm run start
+DOSKEY jamesroot=cd C:\Users\JEmbry\Developer\Freelancing\Test\JamesHenderson\root
+DOSKEY finder=explorer .
+DOSKEY n=start cmd.exe
+DOSKEY sbt="D:\ProgramFiles\Sublime Text 3\sublime_text.exe" $*
+DOSKEY pwd=cd
+DOSKEY pbcopy=clip
+
+DOSKEY vi="D:\ProgramFiles\Vim\vim80\gvim.exe" $*
+DOSKEY vi="D:\ProgramFiles\Vim\vim80\gvim.exe" $*
+
+DOSKEY gp="perl" D:\Developer\logonCommands\perlUtils\gp.pl $*
